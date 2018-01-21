564ad1311ae31457de83603943e1defc9e01df0d
mortuie <jimmybob-leon@hotmail.co.uk>
2018-01-21 19:15:01 +0000

    Adding more of the basic framework

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
 import {css, StyleSheet} from 'aphrodite';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div className={css(styles.background)}>
 
-            </div>
-        );
+    getBar = () => {
+        if (true) {
+            return (
+                <ul className={css(styles.background)}>
+                    <li className={css(styles.title)}><Link className={css(styles.items)} to='/'>Flashcards</Link></li>
+                    <li className={css(styles.nav)}><Link className={css(styles.items)} to='/login'>Login</Link></li>
+                    <li className={css(styles.nav)}><Link className={css(styles.items)} to='/register'>Register</Link></li>
+                </ul>
+            );
+        } else { // logged in....
+
+        }
+    }
+
+    render() {
+        return this.getBar();
     }
 }
 
 const styles = StyleSheet.create({
     background: {
-        backgroundColor: 'black',
+        backgroundColor: '#f4ad42',
         width: '100%',
         height: 50,
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+        listStyleType: 'none',
+    },
+    items: {
+        display: 'block',
+        textAlign: 'center',
+        cursor: 'pointer',
+        textAlign: 'center',
+        padding: '16px 14px',
+        textDecoration: 'none',
+    },
+    title: {
+        float: 'left',
+    },
+    nav: {
+        float: 'right',
     },
 });
--- frontend/src/User/Login.js
+++ frontend/src/User/Login.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
+import {css, StyleSheet} from 'aphrodite';
 
 export default class Login extends Component {
 
+    state = {
+        username: '',
+        password: ''
+    };
+
+    login = () => {
+        console.log(this.state.username);
+    }
 
     render() {
         return (
-            <div>LOGIN PAGE</div>
+            <div className={css(styles.container)}>
+                <input placeholder="example@email.com" onChange={e => this.setState({username: e.target.value})} value={this.state.username}></input>
+                <input type="password" placeholder="*****" onChange={e => this.setState({password: e.target.value})} value={this.state.password}></input>
+                <button onClick={this.login}>Sign in</button>
+            </div>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 'auto',
+        marginTop: 150,
+        height: 200,
+        width: 300,
+        border: '1px solid black',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+});
