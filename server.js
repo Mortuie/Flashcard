b72bd422a1671a516b5d1e72b2b8cb4aecf4518d
mortuie <jimmybob-leon@hotmail.co.uk>
2018-02-12 22:49:38 +0000

    Adding some of the backend

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 var express = require('express');
 var app = express();
 var http = require('http');
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI);
+var db = mongoose.connection;
+db.on('error', (err) => console.log(err));
+db.once('open', () => console.log('DB Connected'));
 
-var PORT = 3001;
+var PORT = process.env.PORT;
 
 app.get("/", (res, req) => {
     req.send("XD");
